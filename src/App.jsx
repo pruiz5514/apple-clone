c03e07daf3b61ce597d15ba0c0b97517cb787a41
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-10-08 20:35:17 -0500

    feat: navbar is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
 
 function App() {
- 
-
   return (
-    <h1>oee</h1>
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
   )
 }
 
